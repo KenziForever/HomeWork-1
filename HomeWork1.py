0425515aa976ca7728854fb0cb68c29ff94228ae
KenziForever <kushibaisagi@gmail.com>
2024-09-06 19:07:10 +0600

    hm.2

--- HomeWork1.py
+++ HomeWork1.py
@@ -8,6 +8,7 @@ class SuperHero:
         self.superpower = superpower
         self.health_points = health_points
         self.catchphrase = catchphrase
+        self.flying = False
 
     def get_name(self):
         return self.name
@@ -16,26 +17,24 @@ class SuperHero:
         self.health_points *= 2
 
     def __str__(self):
-        return f'Nickname: {self.nickname}, Superpower: {self.superpower}, Health Points: {self.health_points}'
+        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health Points: {self.health_points}"
 
-    def catchphrase_length(self):
+    def __len__(self):
         return len(self.catchphrase)
 
-    def __repr__(self):
-        return f'Catchphrase: {self.catchphrase}'
+    def speak(self):
+        return self.catchphrase
 
+hero = SuperHero(
+    name='Clark Kent',
+    nickname='Superman',
+    superpower='Flight and Super Strength',
+    health_points=100,
+    catchphrase='I am the greatest hero of all time!'
+)
 
-if __name__ == '__main__':
-    hero = SuperHero(name='Clark Kent', nickname='Superman', superpower='Flight', health_points=100,
-                     catchphrase='Truth, Justice, and the American Way!')
-
-    print('Hero\'s Name:', hero.get_name())
-
-    hero.double_health()
-    print('Hero\'s Health Points after doubling:', hero.health_points)
-
-    print(hero)
-
-    print(hero.__repr__())
-
-    print('Catchphrase Length:', hero.catchphrase_length())
+print(hero.get_name())
+hero.double_health()
+print(hero)
+print(len(hero))
+print(hero.speak())
